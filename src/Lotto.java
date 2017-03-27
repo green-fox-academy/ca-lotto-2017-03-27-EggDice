1ba3f8e7b6ac0b96ac9670b4bb397841cda8c101
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-03-27 15:40:31 +0200

    refactor(file-read): path stored in a static member

--- src/Lotto.java
+++ src/Lotto.java
@@ -5,9 +5,11 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lotto {
+  final public static String CSV_PATH = "assets/otos.csv";
+
   public static void main(String[] args) {
     try {
-      Path rawCsv = Paths.get("assets/otos.csv");
+      Path rawCsv = Paths.get(CSV_PATH);
       List<String> lines = Files.readAllLines(rawCsv);
     } catch (IOException e) {
       System.out.println("Unable to read the file :(");
