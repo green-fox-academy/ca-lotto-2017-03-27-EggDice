326cafd1f7ccafabec88da0c4d1a7ea6ab24d9ff
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-03-27 15:58:59 +0200

    feat(parse-csv): read all numbers form csv file

--- src/Lotto.java
+++ src/Lotto.java
@@ -3,16 +3,33 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Lotto {
   final public static String CSV_PATH = "assets/otos.csv";
+  final public static String SEPARATOR = ";";
+  final public static int NUMBER_START_INDEX = 11;
+  final public static int LOTTERY_COUNT = 5;
 
   public static void main(String[] args) {
+    List<String> allNumbers = parseAllNumbersFromFile();
+
+    for (String number : allNumbers) {
+      System.out.println(number);
+    }
+  }
+
+  public static List<String> parseAllNumbersFromFile() {
+    List<String> allNumbers = new ArrayList<>();
     List<String> lines = readFileLines();
     for (String line : lines) {
-      System.out.println(line);
+      String[] values = Arrays.copyOfRange(line.split(SEPARATOR), NUMBER_START_INDEX, NUMBER_START_INDEX + LOTTERY_COUNT);
+      for (String number : values) {
+        allNumbers.add(number);
+      }
     }
+    return allNumbers;
   }
 
   public static List<String> readFileLines() {
